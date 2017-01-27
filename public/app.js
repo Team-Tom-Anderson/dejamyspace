82cf021201cb03d988fefa91130f17dae3456b14
cunnellp5 <philip.cunnell@colorado.edu>
2017-01-27 11:41:10 -0700

    initial

---
+++ public/app.js
@@ -0,0 +1,14 @@
+(function() {
+   angular.module('app', ['ui.router'])
+       .component('main', {
+           controller: controller,
+           templateUrl: 'mainView.html'
+       })
+
+
+   function controller() {
+     const vm=this;
+
+   }
+
+})();
