be953f8beee6a88d86b736f05aaf346b48bd3783
Chris Houdlette <choudlette@gmail.com>
2017-01-27 14:18:14 -0700

    working on layout page

---
+++ public/layout/layout.service.js
@@ -0,0 +1,20 @@
+(function() {
+   angular.module('app')
+       .component('layout', {
+           controller: controller,
+           templateUrl: '/layout/layout.view.html',
+           bindings: {
+
+           }
+       })
+
+
+
+   function controller() {
+       const vm = this;
+
+
+
+   }
+
+})();
